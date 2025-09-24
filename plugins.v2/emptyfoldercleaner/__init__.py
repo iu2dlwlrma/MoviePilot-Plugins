c9dc76496f787e052a9eb4b09f6640bee6ebcc94
oriecho <oriecho@live.cn>
2025-09-24 19:53:06 +0800

    update

--- plugins.v2/emptyfoldercleaner/__init__.py
+++ plugins.v2/emptyfoldercleaner/__init__.py
@@ -23,7 +23,7 @@ class EmptyFolderCleaner(_PluginBase):
     # 插件图标
     plugin_icon = "clean.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "oriecho"
     # 作者主页
